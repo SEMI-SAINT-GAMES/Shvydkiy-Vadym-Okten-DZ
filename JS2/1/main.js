2d16501b50fb3920e90d73768bd1cc68dc89bd2e
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2023-05-05 01:28:22 +0300

    js2

---
+++ JS2/1/main.js
@@ -0,0 +1,63 @@
+//  <<==Масиви і обʼєкти==>> 
+let arr = [123, 778, 1, 4, 5, true, false, `vadik`, `okten`, 3,14]
+console.log(arr);
+
+//========================================//
+
+
+let Book1 = {
+    title: `It`,
+    pageCount: 1100, 
+    jenre: `Horror`
+
+}
+
+let book2 = {
+    title: `Arc de Triomphe`,
+    pageCount: 564, 
+    jenre: `Hystorical novel`
+}
+let book3 = {
+    title: `Fatherland`,
+    pageCount: 352, 
+    jenre: `Dystopia`
+}
+
+console.log(Book1, book2, book3);
+
+//========================================//
+
+
+let movie1 = {
+    title: `Mission impossible`,
+    timing: 2.09, 
+    jenre: `Blockbuster`,
+    director: {
+        name: `Brad Burd`,
+        age:  65
+    }
+}
+
+let movie2 = {
+    title: `Wolf of Wall street`,
+    timing: 3.12, 
+    jenre: `Autobiography`,
+    director: {
+        name: `Martin Scorsese`,
+        age:  80
+    }
+}
+let movie3 = {
+    title: `Batman: Dark Knight`,
+    timing: 2.32, 
+    jenre: `Superhero movie`,
+    director: {
+        name: `Cristopher Nolan`,
+        age:  52
+    }
+}
+console.log(movie1, movie2, movie3);
+
+  
+
+
---
+++ JS2/2/main.js
@@ -0,0 +1,192 @@
+// <<==Логічні розгалудження==>> 
+
+let a = 1
+if (a !== 0)
+{
+    console.log(`Вірно`);
+}
+else
+{
+    console.log(`Невірно`);
+}
+a = 0
+if (a !== 0)
+{
+    console.log(`Вірно`);
+}
+else
+{
+    console.log(`Невірно`);
+}
+a = -3
+if (a !== 0)
+{
+    console.log(`Вірно`);
+}
+else
+{
+    console.log(`Невірно`);
+}
+
+//====================================//
+
+let time = +prompt(`Скажи час від 0 до 59 хвилин`)
+
+if (time >= 0 && time <= 59)
+{
+    if (time <= 15)
+    {
+        console.log(`Перша чверть`);
+    }
+    else if (time <= 30)
+    {
+        console.log(`Друга чверть`);
+    }
+    else if (time <= 45)
+    {
+        console.log(`Третя чверть`);
+    }
+    else
+    {
+        console.log(`Четверта чверть`);
+    }
+
+}
+else
+{
+    console.log(`Стільки хвилин не існує`);
+}
+
+//====================================//
+
+let day = +prompt(`Введи число місяця ві 1 до 31`)
+if (day > 0 && day <= 31)
+{
+    if (day <= 10) {
+        console.log(`Перша декада`);
+       }
+    else  if (day <= 20) {
+        console.log(`Друга декада`);
+       }
+    else{
+        console.log(`Третя декада`);
+       }   
+
+    
+}
+else{
+    console.log(`Не буває стількі днів`);
+}
+
+
+//====================================//
+
+let dayNum = +prompt(`Enter day number`)
+
+   switch(dayNum){
+        case 1:
+          console.log(`Tennis`);
+          break;
+        case 2:
+          console.log(`Chess`);
+          break;
+        case 3:
+          console.log(`Library`);
+          break;
+        case 4:
+          console.log(`Jogging`);
+          break;
+        case 5:
+          console.log(`Js lesson`);
+          break;
+        case 6:
+          console.log(`Swimming`);
+          break;
+        case 7:
+          console.log(`Relax`);
+          break;  
+        default:
+            console.log(`Incorrect day number`);
+
+    }
+
+
+
+
+   
+
+
+//====================================//
+
+let a1 = +prompt(`Enter first number`)
+let a2 = +prompt(`Enter second number`)
+
+if (!isNaN(a1) && !isNaN(a2))
+{
+  if ( a1 === a2)
+   {
+      console.log(`equal`);
+   }
+  else if (a1 > a2)
+   {
+      console.log(a1 + `  First is bigger`);
+   }
+  else if (a2 > a1)
+   {
+      console.log(a2 + `  Second is bigger`);
+   }
+}
+else{
+    console.log(`One of numbers is incorret, try again!`)
+}
+
+
+ //====================================//
+
+ let b = prompt(`Enter anything`)
+ if (b === null || b === `` || b === 0 || b === -0 || b === false || b === undefined)
+ {
+    b = `Default`
+    console.log(b);
+ }
+
+ //====================================//
+
+ let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration > 5)
+{
+    console.log(coursesAndDurationArray[0].title + ` - Super!!!`);
+}
+if (coursesAndDurationArray[1].monthDuration > 5)
+{
+    console.log(coursesAndDurationArray[1].title + ` - Super!!!`);
+}
+if (coursesAndDurationArray[2].monthDuration > 5)
+{
+    console.log(coursesAndDurationArray[2].title + ` - Super!!!`);
+}
+if (coursesAndDurationArray[3].monthDuration > 5)
+{
+    console.log(coursesAndDurationArray[3].title + ` - Super!!!`);
+}
+if (coursesAndDurationArray[4].monthDuration > 5)
+{
+    console.log(coursesAndDurationArray[4].title + ` - Super!!!`);
+}
+if (coursesAndDurationArray[5].monthDuration > 5)
+{
+    console.log(coursesAndDurationArray[5].title + ` - Super!!!`);
+}
+
+ 
+
+
+
