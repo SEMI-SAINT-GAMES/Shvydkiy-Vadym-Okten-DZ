eeccc0f68d9249da6b74457b78d7ebe9f2e77536
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2023-06-01 23:57:49 +0300

    10.2

--- JS/JS10/2/main.js
+++ JS/JS10/2/main.js
@@ -6,30 +6,22 @@ let date = new Date()
 
 let sessionCounter = localStorage.getItem(`Updates Counter`)
 console.log(sessionCounter)
-let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-localStorage.setItem(`time${sessionCounter}`, time)
+let time = `date - 0${date.getDate()}/0${date.getMonth()}/${date.getFullYear()},  time - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+//localStorage.setItem(`time${sessionCounter}`, time)
 sessionCounter++
 localStorage.setItem(`Updates Counter`, sessionCounter) 
-// =========================
-// зробити масив на 100 об'єктів та дві кнопки prev next
-// при завантажені сторінки з'являються перші 10 об'єктів.
-// При натисканні next виводяться настпні 10 об'єктів
-// При натисканні prev виводяться попередні 10 об'єктів
+if (localStorage.getItem(`sessionsArr`) === null)
+{
+    localStorage.setItem(`sessionsArr`,JSON.stringify([]))
+    console.log(`empty`)
+}
+
+let ses = localStorage.getItem(`sessionsArr`)
 
 
+let sessionsArr = JSON.parse(ses);
+console.log(sessionsArr)
+sessionsArr.push(time)
+localStorage.setItem(`sessionsArr`, JSON.stringify(sessionsArr))
 
 
-// - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
-
-
-// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
-
-
-// *** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
-// При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
-// (Додатковачастина для завдання)
-
-// *** (подібне було вище, але...будьте уважні в другій частині) створити сторінку з довільним блоком, в середині якого є значення "100грн"
-// при перезавантаженні сторінки до значаення додається по 10грн, але !!!
-//  зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
-//  При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
--- JS/JS10/2/sessions.js
+++ JS/JS10/2/sessions.js
@@ -1,2 +1,14 @@
-let div = document.body.getElementsByTagName("div");
-console.log(div)
+// let div = document.body.getElementsByTagName("div");
+// console.log(div)
+sessionCounter = localStorage.getItem(`Updates Counter`)
+let sessionsArr =JSON.parse(localStorage.getItem(`sessionsArr`))
+
+for (let i = 0; i < sessionCounter; i++)
+{
+   let div =document.createElement(`div`);
+   document.body.appendChild(div)
+   div.innerText =  `Session №${i + 1} : ${sessionsArr[i]}`
+   
+}
+
+
