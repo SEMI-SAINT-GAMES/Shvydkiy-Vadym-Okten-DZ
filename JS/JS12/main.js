0a529ffeaf72d14d0b1acbe2ff8ca406284481cd
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2023-06-12 16:39:08 +0300

    12

---
+++ JS/JS12/main.js
@@ -0,0 +1,33 @@
+// зробити файл users.html
+// з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
+let url = new URL("https://jsonplaceholder.typicode.com/users");
+let usersArr = []
+fetch(url)
+  .then(value => value.json())
+  .then(value => {
+    const userList = document.getElementById('user-list');
+// вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
+    value.forEach(user => {
+
+      const li = document.createElement('li');
+      li.innerText = `${user.id} - ${user.name}`;
+
+     
+      const link = document.createElement('a');
+      link.href = `user-details.html?id=${user.id}`;
+      link.innerText = link.href;
+
+     
+      li.appendChild(link);
+
+     
+      userList.appendChild(li);
+    });
+  })
+
+
+
+
+
+// при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів) отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   ХХХ - айді користувача)
+
